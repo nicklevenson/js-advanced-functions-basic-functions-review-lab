0b4554d4691feccc423cf11c4cfe439b70b1a57b
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-02 13:43:49 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office') {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flare = `*`) {
+  return function(param = 'default') {
+    return `You are ${flare + param + flare}!`
+  }
+}
+
+const Calculator = {
+  add: function(a, b) {
+    return a + b
+  },
+  subtract: function(a, b) {
+    return a - b
+  },
+  multiply: function(a, b) {
+    return a * b
+  },
+  divide: function(a, b) {
+    return a / b
+  }
+}
+
+function actionApplyer(start, functions) {
+  if (functions == false) {
+    return start
+  } else {
+    return functions[start]
+  }
+}
